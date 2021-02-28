099c43c3d5bb8fda3b0840beee183c38e70f6945
Varun Sharma <varunge@gmail.com>
2021-02-28 18:10:27 +0530

    Fixed linting error reported by cookstyle

--- cookbooks/tomcat/recipes/default.rb
+++ cookbooks/tomcat/recipes/default.rb
@@ -5,4 +5,3 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 include_recipe 'tomcat::install_java'
 include_recipe 'tomcat::install_tomcat'
-
--- cookbooks/tomcat/recipes/install_tomcat.rb
+++ cookbooks/tomcat/recipes/install_tomcat.rb
@@ -2,14 +2,16 @@ group 'tomcat'
 
 directory '/opt/tomcat'
 
-user 'tomcat' do 
+user 'tomcat' do
   gid 'tomcat'
   home '/opt/tomcat'
   shell '/bin/nologin'
   action :create
 end
 
-remote_file '/tmp/tomcat.tar.gz' do 
+tmp_tomcat_file = Chef::Config[:file_cache_path] + '/tomcat.tar.gz'
+
+remote_file tmp_tomcat_file do
   # source 'http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz' => this is not working ...404
   source 'https://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz' # use attiribute to get the tomcat version
   action :create
@@ -26,7 +28,7 @@ execute 'extract_tomcat' do
   cwd '/tmp'
 end
 
-execute 'update_permission' do 
+execute 'update_permission' do
   cwd '/opt/tomcat'
   command 'sudo chgrp -R tomcat /opt/tomcat'
   command 'sudo chmod -R g+r conf'
